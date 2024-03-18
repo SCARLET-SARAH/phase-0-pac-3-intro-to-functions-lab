ba5f379d524eeac9f3c67d4cf2dde53bbe8e03c6
scarlet <sarahscarlet641@gmail.com>
2024-03-18 10:54:30 +0300

    create js.lab

---
+++ index.js
@@ -0,0 +1,19 @@
+describe("shout(string)", function () {
+    it("receives one argument and returns it in all caps", function () {
+      expect(shout("hello")).toEqual("HELLO");
+    });
+  });
+
+function shout(string) {
+    // todo
+ 
+}
+  function shout(string) {
+    return string;
+  }
+  "Hello!".toUpperCase(); // 'HELLO!'
+  function shout(string) {
+    return string.toUpperCase();
+  }
+
+
